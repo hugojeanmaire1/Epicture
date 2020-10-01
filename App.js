6731d1e398253c5cf0bb90544f918f6d1c07ef49
Hugo Jeanmaire <hugo.jeanmaire@epitech.eu>
2020-10-01 13:50:10 +0200

    [Adding] src/login
    Adding src/Login component and env file with data

--- App.js
+++ App.js
@@ -8,19 +8,7 @@
 
 import React, {Component} from 'react';
 import {StyleSheet, View, Text, Button} from 'react-native';
-import {authorize} from 'react-native-app-auth';
-
-const config = {
-  issuer: 'https://api.imgur.com/3/',
-  clientId: 'b9aac4c2e7aee5f',
-  clientSecret: '88e8af25253fadd26604147a1f4be86d4e9a90fd',
-  redirectUrl: 'com.epicture://callback',
-  serviceConfiguration: {
-    authorizationEndpoint: 'https://api.imgur.com/oauth2/authorize',
-    tokenEndpoint: 'https://api.imgur.com/oauth2/token',
-    revocationEndpoint: 'https://api.imgur.com/oauth2/revoke',
-  },
-};
+import Login from './src/Login'
 
 export default class App extends Component {
   constructor(props) {
@@ -33,35 +21,16 @@ export default class App extends Component {
     };
   }
 
-  _sendRequestLogin = async () => {
-    try {
-      const result = await authorize(config);
-      console.log(result);
-    } catch (error) {
-      console.log('Error = ' + error);
-    }
-  };
+
 
   render() {
     return (
-      <View style={{flex: 1}}>
         <View style={styles.container}>
-          <View style={{flex: 4}} />
-          <View style={{flex: 4}}>
-            <Button
-              title="Authorize"
-              onPress={() => this._sendRequestLogin()}
-              style={styles.loginButton}
-            />
-          </View>
-          <View style={{flex: 1}}>
-            <Text style={styles.loginText}>
-              Your informations wont be saved or sold.
-            </Text>
-            <Text style={styles.loginText}>202000000000000000</Text>
+          <Text style={styles.loginText}>Salut</Text>
+          <View>
+            <Login/>
           </View>
         </View>
-      </View>
     );
   }
 }
@@ -88,7 +57,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#89C623',
   },
   loginText: {
-    color: 'white',
+    color: 'black',
     alignSelf: 'center',
   },
 });
---
+++ src/Login.js
@@ -0,0 +1,49 @@
+import React, {Component} from 'react';
+import env from '../env.json'
+import {authorize} from 'react-native-app-auth';
+import {Button, StyleSheet, Text, View} from 'react-native';
+
+const config = {
+    issuer: env.issuer,
+    clientId: env.clientId,
+    clientSecret: env.clientSecret,
+    redirectUrl: env.redirectUrl,
+    serviceConfiguration: {
+        authorizationEndpoint: 'https://api.imgur.com/oauth2/authorize',
+        tokenEndpoint: 'https://api.imgur.com/oauth2/token',
+        revocationEndpoint: 'https://api.imgur.com/oauth2/revoke',
+    },
+};
+
+export default class Login extends Component {
+
+    _sendRequestLogin = async () => {
+        try {
+            const result = await authorize(config);
+            console.log(result);
+        } catch (error) {
+            console.log('Error = ' + error);
+        }
+    };
+
+    render() {
+        return (
+            <Button
+                title="Authorize"
+                onPress={() => this._sendRequestLogin()}
+                style={styles.loginButton}
+            />
+        );
+    }
+}
+
+
+const styles = StyleSheet.create({
+    loginButton: {
+        width: '80%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginLeft: '10%',
+        backgroundColor: '#89C623',
+    },
+});
